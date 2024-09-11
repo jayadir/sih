26af76a9e0b91bd50ceab6561cbe98e1d678d4e4
zylonox <shlok.agarwal0604@gmail.com>
2024-09-12 01:15:51 +0530

    user authentication and api's done

---
+++ server/Routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+import { registerController,loginController, logoutController} from '../Controllers/authController.js';
+
+
+//Register
+router.post('/register',registerController)
+
+router.post('/login',loginController)
+
+router.get("/logout", logoutController);
+
+export default router
+
